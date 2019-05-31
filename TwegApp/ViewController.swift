7910f03179f84a71a9e39061b86e79f47c76a025
Erric Alfajri <erric.alfajri@bukalapak.com>
2019-05-31 15:31:08 +0700

    Initial Commit

---
+++ TwegApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwegApp
+//
+//  Created by Erric Alfajri on 31/05/19.
+//  Copyright © 2019 Erric Alfajri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
